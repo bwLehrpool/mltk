c9c02f2259d0e0873fcc4299419ef6553772240e
Simon Rettberg <simon.rettberg@rz.uni-freiburg.de>
2026-09-24 15:20:11 +0200

    [qemu] Avoid writing invalid -uuid param to xml

--- core/modules/qemu/runvirt-plugin-qemu/src/main/java/org/openslx/runvirt/plugin/qemu/configuration/TransformationGenericUuid.java
+++ core/modules/qemu/runvirt-plugin-qemu/src/main/java/org/openslx/runvirt/plugin/qemu/configuration/TransformationGenericUuid.java
@@ -3,6 +3,8 @@ package org.openslx.runvirt.plugin.qemu.configuration;
 import java.util.Iterator;
 import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openslx.libvirt.domain.Domain;
 import org.openslx.runvirt.plugin.qemu.cmdln.CommandLineArgs;
 import org.openslx.util.Util;
@@ -17,6 +19,11 @@ import org.openslx.virtualization.configuration.transformation.TransformationGen
  */
 public class TransformationGenericUuid extends TransformationGeneric<Domain, CommandLineArgs>
 {
+	/**
+	 * Instance of a logger to log messages.
+	 */
+	private static final Logger LOGGER = LogManager.getLogger( TransformationGenericWrapperScript.class );
+
 	/**
 	 * Name of the configuration transformation.
 	 */
@@ -57,18 +64,44 @@ public class TransformationGenericUuid extends TransformationGeneric<Domain, Com
 		// libvirt uuid the system UUID, as this is what libvirt does by default
 		// This is qemu specific but won't do anything for other hypervisors
 		String systemUuid = null;
+		boolean badUuid = false;
 		for ( Iterator<String> it = config.getQemuCmdlnArguments().iterator(); it.hasNext(); ) {
 			String arg = it.next();
 			if ( "-uuid".equals( arg ) ) {
 				if ( it.hasNext() ) {
 					systemUuid = it.next();
+					try {
+						UUID.fromString( systemUuid );
+					} catch (Exception e) {
+						LOGGER.info( "XML contains bad -uuid argument '" + systemUuid + "'" );
+						systemUuid = null;
+						badUuid = true;
+					}
 				} else {
 					it.remove();
 				}
 			}
 		}
-		if ( Util.isEmptyString( systemUuid ) ) {
+		if ( Util.isEmptyString( systemUuid ) && badUuid ) {
+			// No valid UUID found in cmdline, but at least one invalid one. Force wiping and assignment of new one.
+			if ( Util.isEmptyString( originalUuid ) ) {
+				originalUuid = UUID.randomUUID().toString();
+			}
+			LOGGER.info( "Forcing -uuid " + originalUuid );
+		}
+		if ( Util.isEmptyString( systemUuid ) && !Util.isEmptyString( originalUuid ) ) {
 			// No system-uuid on command line, use old libvirt uuid
+			for ( Iterator<String> it = config.getQemuCmdlnArguments().iterator(); it.hasNext(); ) {
+				// Delete any existing uuid cmdline args
+				String arg = it.next();
+				if ( "-uuid".equals( arg ) ) {
+					if ( it.hasNext() ) {
+						it.remove();
+						it.next();
+					}
+					it.remove();
+				}
+			}
 			config.addQemuCmdlnArgument( "-uuid" );
 			config.addQemuCmdlnArgument( originalUuid );
 		}
